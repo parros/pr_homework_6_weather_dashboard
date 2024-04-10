51f93fd00bac6e0ad2b985a91d8ddf0bc76fdcf9
unknown <parkerosemeyer@gmail.com>
2024-04-10 16:49:08 -0500

    Added future weater info

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,12 @@ const day3 = $('#day1')
 const day4 = $('#day1')
 const day5 = $('#day1')
 
-console.log(dayjs().format())
 // Search city info through API
 function searchInfo(event) {
     event.preventDefault()
     const cityName = searchCity.val()
     cityCurrent.empty()
     cityCurrent.append(cityName)
-    console.log(cityName)
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=09f5fc783fda185655df2bae2ca3196b`)
     .then(function(response){
         return response.json()
@@ -29,27 +27,72 @@ function searchInfo(event) {
         })
         .then(function(json){
             console.log(json.list)
+            // Current Time
             const time = json.list[0].dt * 1000
             const currentDate = new Date(time)
+            console.log(currentDate)
             const currentMonth = currentDate.getMonth() + 1
             const currentDay = currentDate.getDate()
             const currentYear = currentDate.getFullYear()
             cityCurrent.append(` (${currentMonth}/${currentDay}/${currentYear})`)
-            console.log(json.list[0].wind.speed)
-            const currentWind = json.list[0].wind.speed
+
+            // Future Time
+            for (let i = 8; i-1 <json.list.length; i= i+8){
+                const futureTime = json.list[i-1].dt * 1000
+                console.log(futureTime)
+                const futureDate = new Date(futureTime)
+                console.log(futureDate)
+                const futureMonth = futureDate.getMonth() + 1
+                const futureDay = futureDate.getDate()
+                const futureYear = futureDate.getFullYear()
+                $(`#day${i/8}Date`).append(`${futureMonth}/${futureDay}/${futureYear}`)
+            }
+
+
+            // Current Wind Speed
+            const currentWindSpeed = json.list[0].wind.speed
+            $('#currentWind').append(currentWindSpeed)
+
+            // Future Wind Speed
+            for (let i = 8; i-1 <json.list.length; i= i+8){
+                const futureWindSpeed = json.list[i-1].wind.speed
+                $(`#day${i/8}Wind`).append(futureWindSpeed)
+            }
+
+            // Current Temp
             const tempInK = json.list[0].main.temp
-            tempInF = (tempInK-273.15)*1.8+32
+            const tempInF = (tempInK-273.15)*1.8+32
             const currentTemp = tempInF.toFixed(2)
-            console.log(currentTemp)
+            $('#currentTemp').append(currentTemp)
+
+            for (let i = 8; i-1 <json.list.length; i= i+8){
+                const futureTempInK = json.list[i-1].main.temp
+                const futureTempInF = (futureTempInK-273.15)*1.8+32
+                const futureTemp = futureTempInF.toFixed(2)
+                $(`#day${i/8}Temp`).append(futureTemp)
+            }
+
+            // Current Humidity
             const currentHumid = json.list[0].main.humidity
-            console.log(json.list[0].main.humidity)
-            console.log(json.list[0].weather[0].icon)
+            $('#currentHumid').append(currentHumid)
+
+            // Future Humidity
+            for (let i = 8; i-1 <json.list.length; i= i+8){
+                const futureHumid = json.list[i-1].main.humidity
+                $(`#day${i/8}Humid`).append(futureHumid)
+            }
+
+            // Current Weather Icon
             const currentIcon = json.list[0].weather[0].icon
-            const newIcon = $(`<img height='56' width='56' src="https://openweathermap.org/img/wn/${currentIcon}@2x.png">`)
-            console.log(cityCurrent)
-            console.log(newIcon)
-            cityCurrent.append(newIcon)
-            console.log(cityCurrent)
+            const addIcon = $(`<img height='56' width='56' src="https://openweathermap.org/img/wn/${currentIcon}@2x.png">`)
+            cityCurrent.append(addIcon)
+
+            // Future Humidity
+            for (let i = 8; i-1 <json.list.length; i= i+8){
+                const futureIcon = json.list[i-1].weather[0].icon
+                const futureAddIcon = $(`<img height='56' width='56' src="https://openweathermap.org/img/wn/${futureIcon}@2x.png">`)
+                $(`#day${i/8}Icon`).append(futureAddIcon)
+            }
         })
     })
 }
